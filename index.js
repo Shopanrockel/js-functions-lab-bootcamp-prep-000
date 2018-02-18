fc27a9a275af1dff83c528c7727132152ff07cf8
Skip Harris <skip.harris@barclaycollege.edu>
2018-02-18 23:07:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(name, holiday) {
-name = "you"
-holiday = "holidays"
-  return `Happy ${holidays}, ${name}!`
+function happyHolidaysTo(name) {
+name = "Skip"
+
+  return `Happy holidays, ${name}!`
   }
   
